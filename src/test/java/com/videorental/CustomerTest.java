809bc1fb49cf8e10b71db93628fde74b2faacbcd
ddaumm <dlaekdna4862@gmail.com>
2026-04-01 15:14:36 +0900

    feature/GoldenMaster Branch Test

--- src/test/java/com/videorental/CustomerTest.java
+++ src/test/java/com/videorental/CustomerTest.java
@@ -9,5 +9,35 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerTest {
+    private static Customer customer;
+    private static final String CUSTOMER_NAME = "name_not_important";
+    private static final String MOVIE_NAME = "movie_name";
+    private static final int NEW_RELEASE = 1;
 
+    @BeforeEach
+    void beforeEach() {
+        customer = new Customer(CUSTOMER_NAME);
+    }
+
+    @Test
+    public void returnNewCustomer() {
+        assertThat(customer).isNotNull();
+    }
+
+    @Test
+    public void statementForNotRental()  {
+        String statement = customer.statement();
+
+        assertThat(statement).isEqualTo(
+                "Rental Record for WHO\n" +
+                        "Amount owed is 0.0\n" +
+                        "You earned 0 frequent renter pointers"
+        );
+    }
+
+    @Test
+    public void statementForRegularMovieRentalForLessThan3Days() {
+        Movie movie = new Movie(MOVIE_NAME, Movie.NEW_RELEASE);
+
+    }
 }
