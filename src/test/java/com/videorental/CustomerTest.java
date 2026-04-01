5a046fe2a0b69ee2d8a0ec62cfafe0c29f3f5868
ddaumm <dlaekdna4862@gmail.com>
2026-04-01 15:09:55 +0900

    INIT

---
+++ src/test/java/com/videorental/CustomerTest.java
@@ -0,0 +1,13 @@
+package com.videorental;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomerTest {
+
+}
